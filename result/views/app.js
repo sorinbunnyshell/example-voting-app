80201fe476314e7fc69be6f573144e5587da5de3
Sorin Dumitrescu <sorin.dumitrescu@bunnyshell.com>
2022-06-22 11:46:12 +0300

    Added votes count for each of the options

--- result/views/app.js
+++ result/views/app.js
@@ -23,6 +23,8 @@ app.controller('statsCtrl', function($scope){
          $scope.aPercent = percentages.a;
          $scope.bPercent = percentages.b;
          $scope.total = a + b;
+         $scope.aCount = a;
+         $scope.bCount = b;
        });
     });
   };
